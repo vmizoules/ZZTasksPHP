9fc2663000a163b0f233797fa2397a4ca27a2fd5
vmizoules <dark-vince15@hotmail.fr>
2015-12-10 14:59:40 +0100

    Required login inputs

--- views/loginForm.php
+++ views/loginForm.php
@@ -8,10 +8,10 @@
 			<div class="modal-body">
 				<form class="form col-md-12 center-block" action="<?php echo generateUrl('login')?>" method="post">
 					<div class="form-group">
-						<input type="text" name="login" class="form-control input-lg" placeholder="<?php echo _t("USERLOGIN") ?>" value="<?php echo getUsername() ?>">
+						<input type="text" name="login" class="form-control input-lg" placeholder="<?php echo _t("USERLOGIN") ?>" value="<?php echo getUsername() ?>" required />
 					</div>
 					<div class="form-group">
-						<input type="password" name="password" class="form-control input-lg" placeholder="<?php echo _t("USERPASSWORD") ?>">
+						<input type="password" name="password" class="form-control input-lg" placeholder="<?php echo _t("USERPASSWORD") ?>" required />
 					</div>
 					<div class="form-group">
 						<button class="btn btn-primary btn-lg btn-block"><?php echo _t("SIGNIN") ?></button>
