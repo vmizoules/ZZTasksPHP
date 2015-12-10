d19481f14b3387aff6e942d4c416fe29fc7e76ab
Alex <aleeeeeexx@hotmail.fr>
2015-12-10 14:59:20 +0100

    improve task

--- includes/taskManager.php
+++ includes/taskManager.php
@@ -1,20 +1,29 @@
 <?php
 
-function createTask ($owner, $titre, $taskUser, $text, array $associatedUsers){
+function createTask ($category, $title, $owner,  $affectedUser,  $description){
 	// read global var $app
 	global $app;
 	// check user permission exists
 	if (checkUserPermission($owner, "createTask")==true){
 		//check user exist
-		if(checkUserExists($taskUser)){
-			//add tasks in global array
-			$app['tasks'][] = array(
-				'titre' => $titre,
-				'owner' => $owner,
-				'taskUser'=> $taskUser,
-				'text'=> $text,
-			);
-			saveTasks();
+		if(checkUserExists($affectedUser)){
+			//check category exist
+			if($category=="todo" || $category=="inprogress" || $category=="done"){
+				//add tasks in global array
+				$app['tasks'][] = array(
+					'category'=> $category,
+					'title' => $title,
+					'owner' => $owner,
+					'affectedUser'=> $affectedUser,
+					'description'=> $description,
+				);
+				saveTasks();
+			} else {
+				throw new Exception("Bad category", 3);
+			}
+
+		} else {
+			throw new Exception("Affected User doesn't exist", 2);
 		}
 	} else {
 		throw new Exception("Permission denied", 1);
--- pages/createTask.php
+++ pages/createTask.php
@@ -5,13 +5,12 @@ $formError = false;
 if (isset($_POST["submit"])){
 	$formError = true;
 	// check all field given
-	if(isset($_POST["title"]) && isset($_POST["taskUser"]) && isset($_POST["taskText"])){
+	if(isset($_POST["category"]) && isset($_POST["title"]) && isset($_POST["affectedUser"]) && isset($_POST["description"])){
 		// check fields not empty
 		if(strlen($_POST["title"]) > 0 ){
 			$formError = false;
-			createTask (getUsername(), $_POST["title"], $_POST["taskUser"], $_POST["taskText"], array());
-
-			exit(); // remplacer par un redirect TODO
+			createTask ($_POST["category"], $_POST["title"], getUsername(), $_POST["affectedUser"], $_POST["description"]);
+			die("Ok"); // remplacer par un redirect TODO
 		}
 	}
 }
