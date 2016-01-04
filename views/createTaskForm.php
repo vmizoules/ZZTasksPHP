a14c5e28d6c4e9be2411ce1cd2d87e0c705adf56
MIZOULES Vincent <vincent.mizoules@poste.isima.fr>
2016-01-04 20:05:23 +0100

    Trans missing word

--- views/createTaskForm.php
+++ views/createTaskForm.php
@@ -4,18 +4,16 @@
 	</div>
 
 	<div class="col-md-6">
-		<h3><?php echo _t("ADDTASK") ?> </h3>
+		<h3> <?php echo _t("ADDTASK") ?> </h3>
 
 		<?php if($formError) { ?>
 			<strong>Bad submit!</strong>
 		<?php } ?>		
 
 		<form class="task-form" action="<?php echo generateUrl('createTask')?>" method="post">
-
 			<!-- Form -->
 			<?php include("views/components/taskForm.php"); ?>
-
-			<input type="submit" class="btn btn-success" name="submit" />
+			<input type="submit" class="btn btn-success" name="submit" value="<?php echo _t("VALIDATE") ?>" />
 		</form>
 	</div>
 
--- views/updateTaskForm.php
+++ views/updateTaskForm.php
@@ -4,17 +4,16 @@
 	</div>
 
 	<div class="col-md-6">
-		<h3><?php echo _t("EDITTASK") ?> </h3>
+		<h3> <?php echo _t("EDITTASK") ?> </h3>
 
 		<?php if($formError) { ?>
 			<strong>Bad submit!</strong>
 		<?php } ?>		
-			
 
 		<form class="task-form" action="<?php echo generateUrl('updateTask')."&id=".$id ?>" method="post">
 			<!-- Form -->
 			<?php include("views/components/taskForm.php"); ?>
-			<input type="submit" class="btn btn-success" name="submit" />
+			<input type="submit" class="btn btn-success" name="submit" value="<?php echo _t("VALIDATE") ?>" />
 		</form>
 	</div>
 
