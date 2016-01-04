9b06d4d9d443c6708e74c28efd42fdec20e10de6
Alexandre <alexandre.barret@poste.isima.fr>
2016-01-04 18:52:13 +0100

    add stat task homepage

---
+++ views/homepage.php
@@ -0,0 +1,9 @@
+<?php
+//given vars: $total $todo $inprogress $done
+?>
+
+<p> Nombre Total tache: <?php echo $total ?></p>
+
+<p>Nombre de tache à faire : <?php echo $todo ?></p>
+<p>Nombre de tache en cours : <?php echo $inprogress; ?></p>
+<p>Nombre de tache terminée : <?php echo $done; ?></p>
