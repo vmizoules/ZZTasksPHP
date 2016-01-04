4f295754eaea09bd6b141d038dd8138d5695d9ff
Alexandre <alexandre.barret@poste.isima.fr>
2016-01-04 19:26:27 +0100

    Update Homepage

--- views/homepage.php
+++ views/homepage.php
@@ -1,9 +1,22 @@
 <?php
 //given vars: $total $todo $inprogress $done
 ?>
+<div class="row text-center">
+	<div class="count-total col-lg-12">
+		<p> Nombre Total tache: <?php echo $total ?></p>
+	</div>
+</div>
 
-<p> Nombre Total tache: <?php echo $total ?></p>
+<div class="row text-center">
+	<div class="count-todo col-lg-4">
+		<p>Nombre de tache à faire : <?php echo $todo ?></p>
+	</div>
 
-<p>Nombre de tache à faire : <?php echo $todo ?></p>
-<p>Nombre de tache en cours : <?php echo $inprogress; ?></p>
-<p>Nombre de tache terminée : <?php echo $done; ?></p>
+	<div class="count-inprogress col-lg-4">
+		<p>Nombre de tache en cours : <?php echo $inprogress; ?></p>
+	</div>
+
+	<div class="count-done col-lg-4">
+		<p>Nombre de tache terminée : <?php echo $done; ?></p>
+	</div>
+</div>
