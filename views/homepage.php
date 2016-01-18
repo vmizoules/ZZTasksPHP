ad5792741d7bb58c80712448230a33d7b84d1eab
unknown <vimizoules1@tsefc.rcisima.isima.fr>
2016-01-18 17:46:18 +0100

    Add link on homepage

--- views/homepage.php
+++ views/homepage.php
@@ -4,24 +4,26 @@
 
 <div class="count-stats">
 
-	<div class="row text-center">
-		<div class="count total col-lg-12 container">
-			<p><?php echo _t("TOTAL") ?><strong><?php echo $total ?></strong></p>
-		</div>
-	</div>
-
-	<div class="row text-center">
-		<div class="count todo col-lg-4 container">
-			<p><?php echo _t("TODOLIST") ?> <strong><?php echo $todo ?></strong></p>
+	<a href="<?php echo generateUrl('listTask')?>">
+		<div class="row text-center">
+			<div class="count total col-lg-12 container">
+				<p><?php echo _t("TOTAL") ?><strong><?php echo $total ?></strong></p>
+			</div>
 		</div>
 
-		<div class="count inprogress col-lg-4 container">
-			<p><?php echo _t("INPROGRESSLIST") ?> <strong><?php echo $inprogress; ?></strong></p>
-		</div>
+		<div class="row text-center">
+			<div class="count todo col-lg-4 container">
+				<p><?php echo _t("TODOLIST") ?> <strong><?php echo $todo ?></strong></p>
+			</div>
 
-		<div class="count done col-lg-4 container">
-			<p><?php echo _t("DONE") ?> <strong><?php echo $done; ?></strong></p>
+			<div class="count inprogress col-lg-4 container">
+				<p><?php echo _t("INPROGRESSLIST") ?> <strong><?php echo $inprogress; ?></strong></p>
+			</div>
+
+			<div class="count done col-lg-4 container">
+				<p><?php echo _t("DONE") ?> <strong><?php echo $done; ?></strong></p>
+			</div>
 		</div>
-	</div>
+	</a>
 
 </div>
