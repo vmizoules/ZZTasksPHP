b9e3785dc7a5b43d6558d2f255024b3588002dc9
MIZOULES Vincent <vincent.mizoules@poste.isima.fr>
2016-01-18 18:44:09 +0100

    Redesign tasks

--- views/components/task.php
+++ views/components/task.php
@@ -1,28 +1,34 @@
 <div class="row">   
 	<br>
 	<div class="task-content col-sm-12">
-		<h3 class="task-title"><?php echo $value["title"] ?></h3>
+
+
+		<div class="row">
+			<div class="col-xs-9">
+				<h4><span class="label label-default"> <?php echo _t("AFFECTEDTOTASK") ?><?php echo $value["affectedUser"] ?></span></h4>
+				
+				<h3 class="task-title"><?php echo $value["title"] ?></h3>
+				
+				<?php echo $value["description"] ?><br/>
+
+				<small><em>(<?php echo _t("OWNERTASK") ?><?php echo $value["owner"] ?>)</em></small>
+				<br/><br>
+			</div>
+			<div class="col-xs-3"></div>
+		</div>
 
 		<?php if(checkUserPermission(getUsername(), 'editTask')) { // if user have edit permissions ?>
 			<a href="<?php echo generateUrl('updateTask').'&id='.$key ?>">
-				<i class="glyphicon glyphicon-edit"> </i>
+				<i class="glyphicon glyphicon-edit"> </i> <?php echo _t("MODIFYTASK") ?>
 			</a>
 		<?php } ?>
 		<?php if(checkUserPermission(getUsername(), 'removeTask')) { // if user have remove permissions -> display the modal ?>
 			<a href="#" data-toggle="modal" data-target="#confirm-delete-<?php echo $key ?>">
-				<i class="glyphicon glyphicon-remove-circle"> </i>
+				<i class="glyphicon glyphicon-remove-circle"> </i> <?php echo _t("DELETETASK") ?>
 			</a>
 		<?php } ?>
 
-		<div class="row">
-			<div class="col-xs-9">
-				<small style="font-family:courier,'new courier';" class="text-muted"><?php echo $value["description"] ?></small>
-				<h4><span class="label label-default"> <?php echo _t("AFFECTEDTOTASK") ?><?php echo $value["affectedUser"] ?></span></h4><h4>
-				<h4><span class="label label-default"> <?php echo _t("OWNERTASK") ?><?php echo $value["owner"] ?></span></h4>
-				</div>
-			<div class="col-xs-3"></div>
-		</div>
-		<br><br>
+		<br>
 	</div>
 </div>
 
