00f69c1201c9b73476fb4abf37d936302d6e4fb4
Alex <aleeeeeexx@hotmail.fr>
2016-01-21 11:30:27 +0100

    test hash password

--- tests/includes/authenticationTest.php
+++ tests/includes/authenticationTest.php
@@ -36,4 +36,13 @@ class authenticationTest extends PHPUnit_Framework_TestCase {
 		// TODO
 	}
 
+	public function testHashPassword (){
+		// empty value
+		$this->assertEquals(hashPassword('',''),'202cb962ac59075b964b07152d234b70');
+		
+		// credible value
+		$this->assertEquals(hashPassword("test","test"), '03698509376aacc7c2c4c7a8e4e77ea2');
+
+	}
+
 }
