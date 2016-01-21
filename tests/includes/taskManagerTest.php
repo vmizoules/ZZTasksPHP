20b6d80d7b7c599e75d0cd7e0bd320e786190855
vmizoules <dark-vince15@hotmail.fr>
2016-01-21 11:28:23 +0100

    Add global var on all test files

--- tests/includes/taskManagerTest.php
+++ tests/includes/taskManagerTest.php
@@ -3,6 +3,18 @@
 include_once('includes/autoload.php');
 
 class taskManagerTest extends PHPUnit_Framework_TestCase {
+	
+	// set up global app var
+	public function setUp() {
+		// init global app var
+		global $app;
+		$app['param'] = getConfig("test");
+		$app['users'] = loadJson($app['param']['db_path_users']);
+		$app['pages'] = loadJson($app['param']['db_path_pages']);
+		$app['tasks'] = loadJson($app['param']['db_path_tasks']);
+		$app['trans'] = "testLang";
+	}
+
 	public function testCreateTask() {
 		// TODO
 	}
