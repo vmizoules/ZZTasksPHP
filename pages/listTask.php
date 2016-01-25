5d9d5f25b07eb1c1332c989c96e1b0b4f25e9d70
MIZOULES Vincent <vincent.mizoules@poste.isima.fr>
2016-01-25 18:40:07 +0100

    Fix listTask permission

--- pages/listTask.php
+++ pages/listTask.php
@@ -1,3 +1,10 @@
 <?php
 
-include_once('views/listTask.php');
+// check user permission exists
+if (checkUserPermission(getUsername(), "listTask")) {
+	// display list
+	include_once('views/listTask.php');
+} else {
+    // user do not have permission -> display to him
+    include_once('views/permissionError.php');
+}
